95afa573872e7b0bdb0d4671431dbf92480efd2d
Suraj Deshmukh <suraj.deshmukh@microsoft.com>
2025-03-24 11:06:03 -0700

    test: Add scripts
    - Add a bash script that tests generic IB.
    - Add a python script that tests GPU based IB.
    
    Signed-off-by: Suraj Deshmukh <suraj.deshmukh@microsoft.com>

---
+++ tests/scenarios/k8s/base/test-scripts/rdma_test.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+# RDMA Over InfiniBand Test Script
+# Usage:
+#   - Server: ./rdma_test.sh server
+#   - Client: ./rdma_test.sh client leader
+
+set -euo pipefail
+
+# Check if the DEBUG env var is set to true
+if [ "${DEBUG:-false}" = "true" ]; then
+    set -x
+else
+    echo "Debug mode is disabled. Set env var 'DEBUG=true' to enable debug mode."
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+# Set the RDMA port
+export PORT=18515
+
+function get_ib_devices() {
+    # Get the list of available IB devices. There is a possibility that the node
+    # has a NIC that shows up as mlx but is not an IB device. It could be using
+    # Ethernet at the link layer. So ignore such devices.
+    #
+    # One way to ignore such devices at NCCL level is to set "export
+    # NCCL_IB_HCA=^mlx5_8". So here we are ignoring the mlx5_8 device.
+    # See the failures like this: https://gist.github.com/surajssd/d65a9eb3844bfc49ccda3e84a0ff5a4b#file-nccl-failure-on-roce-mlx5_8-device-sh
+    IB_DEVICES=$(for dev in /sys/class/infiniband/*; do
+        hca=$(basename "$dev")
+        if [[ $(cat $dev/ports/1/link_layer 2>/dev/null) == "InfiniBand" ]]; then
+            echo "$hca"
+        fi
+    done)
+
+    # Check if IB devices exist
+    if [[ -z "$IB_DEVICES" ]]; then
+        echo "No InfiniBand devices found!"
+        exit 1
+    fi
+
+    echo "Detected IB Devices:"
+    echo "$IB_DEVICES"
+}
+
+function check_tools() {
+    # Function to check if a command exists
+    command_exists() {
+        command -v "$1" >/dev/null 2>&1
+    }
+
+    # Check for required RDMA tools
+    for cmd in ibstat ibv_rc_pingpong ib_read_lat ib_read_bw; do
+        if ! command_exists "$cmd"; then
+            echo "Error: $cmd not found. Run: apt install -y infiniband-diags ibverbs-utils perftest"
+            exit 1
+        fi
+    done
+}
+
+function check_ib_device_is_active() {
+    # Check if the IB device is active
+    IB_DEVICE_STATE=$(ibstat "$IB_DEVICE" | grep State | cut -d':' -f2 | tr -d '[:space:]')
+    if [[ "${IB_DEVICE_STATE}" != "Active" ]]; then
+        echo "Error: ${IB_DEVICE} is not active!"
+        exit 1
+    fi
+}
+
+get_ib_devices
+check_tools
+
+# Server Mode
+if [[ "$1" == "server" ]]; then
+    for IB_DEVICE in $IB_DEVICES; do
+        echo "Testing RDMA on device: $IB_DEVICE (Port $PORT)"
+
+        check_ib_device_is_active
+
+        echo "Starting RDMA Server 'ibv_rc_pingpong' for $IB_DEVICE (Port $PORT)..."
+        # Run RDMA ping-pong test
+        ibv_rc_pingpong --ib-dev "$IB_DEVICE"
+
+        echo "Starting RDMA Server 'ib_read_lat' for $IB_DEVICE (Port $PORT)..."
+        # Run RDMA latency test
+        ib_read_lat --ib-dev "$IB_DEVICE"
+
+        echo "Starting RDMA Server 'ib_read_bw' for $IB_DEVICE (Port $PORT)..."
+        # Run RDMA bandwidth test
+        ib_read_bw --ib-dev "$IB_DEVICE"
+
+    done
+
+# Client Mode
+elif [[ "$1" == "client" && -n "$2" ]]; then
+    SERVER_IP="$2"
+
+    echo "Starting RDMA Client, connecting to $SERVER_IP..."
+    for IB_DEVICE in $IB_DEVICES; do
+        echo "Testing RDMA on device: $IB_DEVICE (Port $PORT)"
+
+        check_ib_device_is_active
+
+        # Run RDMA ping-pong test
+        # Try this command until the server is ready
+        until ibv_rc_pingpong --ib-dev "$IB_DEVICE" -p $PORT "$SERVER_IP"; do
+            echo "Waiting for 'ibv_rc_pingpong' server to be ready for $IB_DEVICE..."
+            sleep 2
+        done
+
+        # Run RDMA latency test
+        until ib_read_lat --ib-dev "$IB_DEVICE" -p $PORT "$SERVER_IP"; do
+            echo "Waiting for 'ib_read_lat' server to be ready for $IB_DEVICE..."
+            sleep 2
+        done
+
+        # Run RDMA bandwidth test
+        until ib_read_bw --ib-dev "$IB_DEVICE" -p $PORT "$SERVER_IP"; do
+            echo "Waiting for 'ib_read_bw' server to be ready for $IB_DEVICE..."
+            sleep 2
+        done
+
+    done
+
+else
+    echo "Usage: $0 server | client <server_ib_ip>"
+    exit 1
+fi
