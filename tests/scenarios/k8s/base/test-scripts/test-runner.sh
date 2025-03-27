660d37164eba221adcd3b2934c63000dffbf9236
Suraj Deshmukh <suraj.deshmukh@microsoft.com>
2025-03-27 15:19:58 -0700

    tests: Add support for non-GPU root-nic-policy
    Signed-off-by: Suraj Deshmukh <suraj.deshmukh@microsoft.com>

--- tests/scenarios/k8s/base/test-scripts/test-runner.sh
+++ tests/scenarios/k8s/base/test-scripts/test-runner.sh
@@ -6,18 +6,32 @@ set -x
 
 export TESTS_DIR="/root/tests"
 
+# Does not need GPU.
+# Needs IB.
 bash ${TESTS_DIR}/rdma_test.sh ${ROLE} leader
 
-torchrun --nnodes 2 \
-    --nproc-per-node=gpu \
-    --rdzv_backend=static \
-    --rdzv_endpoint=leader:29500 \
-    --node_rank=${TORCH_RUN_RANK} \
-    ${TESTS_DIR}/vllm-rdma.py
+# Needs GPU
+which lspci >/dev/null 2>&1 || {
+    echo "lspci command not found. Please run 'apt install -y pciutils'"
+    exit 1
+}
 
-torchrun --nnodes 2 \
-    --nproc-per-node=gpu \
-    --rdzv_backend=static \
-    --rdzv_endpoint=leader:29500 \
-    --node_rank=${TORCH_RUN_RANK} \
-    ${TESTS_DIR}/verify_gpudirect_rdma.py
+# Check if the pod has NVIDIA GPUs
+if lspci | grep -i nvidia; then
+    torchrun --nnodes 2 \
+        --nproc-per-node=gpu \
+        --rdzv_backend=static \
+        --rdzv_endpoint=leader:29500 \
+        --node_rank=${TORCH_RUN_RANK} \
+        ${TESTS_DIR}/vllm-rdma.py
+
+    torchrun --nnodes 2 \
+        --nproc-per-node=gpu \
+        --rdzv_backend=static \
+        --rdzv_endpoint=leader:29500 \
+        --node_rank=${TORCH_RUN_RANK} \
+        ${TESTS_DIR}/verify_gpudirect_rdma.py
+else
+    echo "No NVIDIA GPUs found. Skipping GPU tests."
+    exit 0
+fi
